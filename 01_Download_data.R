c1fc3f998e455246983932a280ea13410a11f7de
xyees <xyees001@studenti.czu.cz>
2026-07-21 13:32:54 +0200

    Update 01_Download_data.R

--- 01_Download_data.R
+++ 01_Download_data.R
@@ -1,1 +1,25 @@
+library(jsonlite)
 
+doi <- "10.5281/zenodo.20479866"
+record_id <- sub(".*zenodo\\.", "", doi)
+out_dir <- "zenodo_20479866"
+dir.create(out_dir, showWarnings = FALSE, recursive = TRUE)
+api_url <- paste0("https://zenodo.org/api/records/", record_id)
+record <- fromJSON(api_url, simplifyVector = FALSE)
+files <- record$files
+for (f in files) {
+  file_name <- f$key
+  download_url <- f$links$self
+  out_path <- file.path(out_dir, file_name)
+  
+  message("Downloading: ", file_name)
+  
+  download.file(
+    url = download_url,
+    destfile = out_path,
+    mode = "wb",
+    quiet = FALSE
+  )
+}
+
+message("Done")
